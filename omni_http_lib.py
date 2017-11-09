2b31f6da37a117f3b05ba652a583590a7b9c8200
spacebigjam <space.big.jam@gmail.com>
2017-11-09 19:03:52 +0100

    Initial commit

---
+++ omni_http_lib.py
@@ -0,0 +1,16 @@
+import sys
+import logging
+import urllib.error
+import gzip
+
+
+from urllib.request import Request
+from urllib.request import urlopen
+
+class HttpMngmt:
+
+	def __init__(self):
+
+	def run_request(self):
+
+	def load_response(self):
